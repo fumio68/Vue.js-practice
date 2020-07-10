0037cebfaa883926d32ba7af58e0afc01204152a
fumio68 <rgkk.5123@gmail.com>
2020-07-10 14:19:18 +0900

    computedとmethodの使い分け

--- 04v-modelの双方向バインディング/main.js
+++ 04v-modelの双方向バインディング/main.js
@@ -4,13 +4,14 @@ new Vue({
     message: 'こんにちは',
     counter: 0,
   },
-  methods: {
-
-  },
   // dataにはプレーンな値しか設定できないため、動的な値はcomputedプロパティに登録する
+  // methodでも同様な処理はできるが、二重波カッコのなかにmethodを書くと、不必要なタイミング（何かの値が変わって再描画されるたびに）でも反応してしまう
   computed: {
     lessThanThree: function() {
       return this.counter > 3 ? '3より上' : '3以下'
     }
-  }
+  },
+  methods: {
+
+  },
 })
