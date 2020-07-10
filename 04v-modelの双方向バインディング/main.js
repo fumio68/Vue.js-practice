f2d8e1f2e6271a58e9a92fcb10b0b01bc230ce37
fumio68 <rgkk.5123@gmail.com>
2020-07-10 14:05:43 +0900

    computedプロパティ（動的な値を登録）

--- 04v-modelの双方向バインディング/main.js
+++ 04v-modelの双方向バインディング/main.js
@@ -2,8 +2,15 @@ new Vue({
   el: '#app',
   data: {
     message: 'こんにちは',
+    counter: 0,
   },
   methods: {
 
+  },
+  // dataにはプレーンな値しか設定できないため、動的な値はcomputedプロパティに登録する
+  computed: {
+    lessThanThree: function() {
+      return this.counter > 3 ? '3より上' : '3以下'
+    }
   }
 })
