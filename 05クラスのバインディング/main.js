ad3676b97440496375ce75237cb4c17678b00b80
fumio68 <rgkk.5123@gmail.com>
2020-07-10 14:50:17 +0900

    クラスをデータにバインディングする方法（真偽値を指定）

---
+++ 05クラスのバインディング/main.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#app',
+  data: {
+    isActive: true
+  },
+  computed: {
+    classObject: function() {
+      return {
+        red: this.isActive,
+        'bg-blue': !this.isActive }
+    }
+  }
+})
